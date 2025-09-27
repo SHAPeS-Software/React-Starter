f76ea958ff594136b750711f0f4becba06dc1812
SHAPeS-Software <kevinkiwi0078@gmail.com@gmail.com>
2025-09-27 16:50:23 -0500

    Updated JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
 
 function App() {
@@ -8,26 +6,26 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>My Site</h1>
+      <br />
+      <pre>
+        <code>Count is {count}</code>
+        <br />
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <br />
+      </pre>
+      <pre>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
+      Facere suscipit eos temporibus earum minus quam voluptatibus<br />
+      magnam blanditiis iste doloremque perferendis libero, nobis <br />
+      voluptate omnis iure nemo! Dolore, provident aperiam?
+      </pre>
+
+      <hr />
+      <br />
+      <i id="copy">Copyright &copy; {(new Date()).getFullYear()} Your Name</i>
     </>
   )
 }
